58872e05eb4d97096bb678c2d21e67cd4a2a273f
Emmanuel-otieno <sakoemmanuel4@gmail.com>
2021-01-27 17:03:10 +0300

    deployment

--- instagram/settings.py
+++ instagram/settings.py
@@ -89,7 +89,7 @@ WSGI_APPLICATION = 'instagram.wsgi.application'
 
 
 MODE=config("MODE", default="dev")
-
+SECRET_KEY = config('SECRET_KEY')
 DEBUG = config('DEBUG', default=False, cast=bool)
 # development
 if config('MODE')=="dev":
